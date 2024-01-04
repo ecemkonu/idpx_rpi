b03f4a07cc7e34a7911db85823abb84e1c34cdec
ecem.konu <39986402+ecemkonu@users.noreply.github.com>
2024-01-05 00:12:07 +0100

    add joystick

--- LED_test.py
+++ LED_test.py
@@ -1,9 +1,22 @@
 from pyfirmata import Arduino, util
 from time import sleep
 board = Arduino('/dev/ttyUSB0')
-led = 12
-while True:
-    board.digital[led].write(1)
-    sleep(0.2)
-    board.digital[led].write(0)
-    sleep(0.2)
+led = 9
+it = util.Iterator(board) 
+it.start()
+vrx = 0
+vry = 1
+sw = 5
+board.analog[vrx].enable_reporting()
+board.analog[vry].enable_reporting()
+
+while True: 
+     value = board.analog[0].read() 
+     print(value) 
+     if value == None: 
+          value = 0
+     elif value > 0.05: 
+          board.digital[led].write(1) 
+          sleep(value) 
+          board.digital[led].write(0) 
+          sleep(value)

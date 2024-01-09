0292826a06e507a1f4f655e28c094d6f6b73dfc6
ecem.konu <39986402+ecemkonu@users.noreply.github.com>
2024-01-09 11:22:04 +0100

    Update CLI.py
    add button config and leds

--- CLI.py
+++ CLI.py
@@ -1,5 +1,5 @@
 from guizero import App, Box, Text, PushButton, Combo, Slider
-from gpiozero import LED
+from gpiozero import LED, Button
 import random
 
 led_pins = [17, 22, 23]
@@ -9,6 +9,10 @@ buttons = []
 
 for led_pin in led_pins:
     leds.append(LED(led_pin))
+
+    
+for button_pin in button_pins:
+    buttons.append(Button(button_pin))
 
 def light_led_at_random(leds):
     led = random.choice(leds)
@@ -37,6 +41,8 @@ def show_routine(selected_value):
         routine_3()
 
 def diagnostic_1():
+    text4.value= "Step 1: Press Button 1"
+    switch_screen(option4)
 
     ## Press button 1
     ## Press button 3
@@ -44,6 +50,8 @@ def diagnostic_1():
     None
 
 def diagnostic_2():
+    text4.value = "Step 1: Press Button 3"
+    switch_screen(option4)
     ## Controller to up
     ## Press button 2
     ## controller to left
@@ -73,8 +81,10 @@ def routine_3():
 
 def show_diagnostic(selected_value):
     if selected_value == "Yes":
-        text4.value= "Step 1: "
-        switch_screen(option4)
+        if random.random() < .5:
+            diagnostic_1()
+        else:
+            diagnostic_2()
     else:
         switch_screen(option0)
 
@@ -82,18 +92,24 @@ def diagnostic_found():
     text2.value = 'Diagnostic found, do you want to walk through?'
     combo2 = Combo(option2, options=["Yes", "No"], align="bottom", command=show_diagnostic)
 
-app = App("Maintanance Helper", layout="grid")
+app = App("Maintanance Helper")
 
 # Create a blank list to hold all the different screens
 all_screens = []
 
+# Add space at the top of the buttons
+spacer_box = Text(app, text="", width=30, height=1)
+
 # Create a box to contain the menu buttons
-menu = Box(app, grid=[0,0], layout="grid")
+menu = Box(app)
 menu.tk.width = 900
 menu.bg = "light blue"
 
+# Add space at the bottom of the buttons
+spacer_box_2 = Text(app, text="", width=30, height=1)
+
 # Option 0 box
-option0 = Box(app, width='fill', grid=[1,1])
+option0 = Box(app, width='fill')
 text0 = Text(option0, "Welcome to the Maintanance Helper")
 all_screens.append(option0)
 
@@ -107,22 +123,22 @@ combo = Combo(option1, options=["E1", "E2", "E3"], align="bottom", command=show_
 all_screens.append(option1)
 
 # Option 2 box
-option2 = Box(app, grid=[1,1])
+option2 = Box(app)
 text2 = Text(option2, text="Running Diagnostic...")
 all_screens.append(option2)
 
 # Option 3 box
-option3 = Box(app, grid=[1,1])
+option3 = Box(app)
 text3 = Text(option3)
 all_screens.append(option3)
 
 # Option 4 box
-option4 = Box(app, grid=[1,1])
+option4 = Box(app)
 text4 = Text(option4)
 all_screens.append(option4)
 
 # Option 5 box
-option5 = Box(app, grid =[1,1])
+option5 = Box(app)
 text5 = Text(option5)
 text6 = Text(option5)
 text7 = Text(option5)
@@ -130,8 +146,8 @@ button5 = PushButton(option5, text="Complete", command=switch_screen, args=[opti
 all_screens.append(option5)
 
 # Add the screens to the menu box
-option1_button = PushButton(menu, text="Maintanance Routines", command=switch_screen, args=[option1], grid=[0,0], align="left")
-option2_button = PushButton(menu, text="Run Diagnostics", command=switch_screen, args=[option2], grid=[1,0], align="left")
+option1_button = PushButton(menu, text="Maintanance Routines", command=switch_screen, args=[option1], align="left")
+option2_button = PushButton(menu, text="Run Diagnostics", command=switch_screen, args=[option2], align="left")
 
 # Hide all screens and then show the first one
 hide_all()
